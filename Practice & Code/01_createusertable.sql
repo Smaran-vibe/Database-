caf15901c496e7af7422525eb8613414b2785ac9
Smaran <smaranaryal60@gmail.com>
2026-02-14 19:21:22 +0545

    Did some query practice

--- Practice & Code/01_createusertable.sql
+++ Practice & Code/01_createusertable.sql
@@ -1,12 +1,25 @@
-CREATE DATABASE IF NOT EXISTS startersql;
+ CREATE DATABASE IF NOT EXISTS startersql;
 USE startersql;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    gender ENUM('Male', 'Female', 'Other'),
-    date_of_birth DATE,
+     gender ENUM('Male', 'Female', 'Other'),
+     date_of_birth DATE,
     salary DECIMAL(10, 2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+     );
+     
+CREATE TABLE IF NOT EXISTS Clients(
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+name varchar(100) NOT NULL,
+email Varchar(100) UNIQUE NOT NULL,
+gender enum ('MALE','FEMALE','OTHER'),
+date_of_birth DATE,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
 );
--- startersql.sql
+++ startersql.sql
@@ -2,10 +2,67 @@ USE startersql;
 SELECT * FROM users;
 -- SELECT * FROM users ORDER BY date_of_birth ASC;
 -- SELECT * FROM users ORDER BY name DESC;
-
+ 
+ -- ALTER TABLE
  ALTER TABLE users add CONSTRAINT chk_dob CHECK(date_of_birth > 2026) ;
-INSERT INTO users (name,email,gender,date_of_birth,salary) VALUES
-('Samrat','samrat96@gmail.com','Male','2027-09-12',100000);
+-- INSERT INTO users (name,email,gender,date_of_birth,salary) VALUES
+-- ('Samrat','samrat96@gmail.com','Male','2027-09-12',100000);
+ 
+ -- Insert table 
+-- INSERT INTO users (name,email,gender,date_of_birth,salary) VALUES
+-- ('Mohan','Mohan96@gmail.com','Male','2021-09-12',90000);
 
+-- UPDATE users SET email ='Mohan09@gmail.com' WHERE id = 30;
+-- SELECT * FROM users ORDER BY gender ASC;
+
+-- DELETE FROM users WHERE email = 'Mohan09@gmail.com';
+
+SELECT * FROM Clients;
+INSERT INTO Clients (user_id, name, email, gender, date_of_birth)
+VALUES
+(1, 'Ram Shrestha', 'ram1@example.com', 'MALE', '1998-05-14'),
+(1, 'Sita Karki', 'sita1@example.com', 'FEMALE', '2000-09-22'),
+(2, 'Aarav Gurung', 'aarav1@example.com', 'MALE', '1995-12-03');
+
+
+-- DROP TABLE Clients;
+-- SHOW TABLES;
+
+-- Join
+-- SELECT users.name AS user_name,
+-- Clients.name AS client_name
+-- FROM users
+-- JOIN Clients 
+-- ON users.id = Clients.user_id;
+
+SELECT users.id,
+users.name AS user_name,
+Clients.name AS client_name
+FROM users
+LEFT JOIN Clients
+ON users.id = Clients.user_id;
+
+SELECT users.name AS user_name,
+Clients.name AS client_name
+FROM users
+RIGHT JOIN Clients
+ON users.id = Clients.user_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
     
