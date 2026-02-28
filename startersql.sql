3363921ebccb753e8f3208a9166d1be0b13aa365
Smaran <smaranaryal60@gmail.com>
2026-02-28 20:49:27 +0545

    initial commit

--- startersql.sql
+++ startersql.sql
@@ -5,6 +5,8 @@ SELECT * FROM users;
 SELECT gender,avg(salary) as 'Average Salary' FROM users group by gender;
 Select gender as 'Gender',avg (salary) as 'Avg',count(*) as 'Count' 
 From users group by gender
+
+-- Having clause is only used after group by instead of where 
 Having avg(salary) > 65000; 
 
 SHOW indexes FROM users;
