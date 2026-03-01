06502b8885d45591c641cf05dd681c7dc9398f64
Smaran <smaranaryal60@gmail.com>
2026-03-01 21:03:16 +0545

    initial commit

--- startersql.sql
+++ startersql.sql
@@ -4,7 +4,7 @@ SELECT * FROM users;
 
 SELECT gender,avg(salary) as 'Average Salary' FROM users group by gender;
 Select gender as 'Gender',avg (salary) as 'Avg',count(*) as 'Count' 
-From users group by gender WITH ROLLUP  -- rollup is for weightage average -- 
+From users group by gender WITH ROLLUP  -- rollup is for weightage averages -- 
 
 -- Having clause is only used after group by instead of where 
 Having avg(salary) > 65000; 
